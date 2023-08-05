89bdc95e46f14bf85027d1c6b35214bfb267058c
gabrielmina118 <gabrielmina118@gmail.com>
2023-08-05 15:41:40 -0300

    configuração e conexão do banco de dados

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { connectionDB } from "./data/dbConnect.js";
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+connectionDB.once("open",()=>{
+  console.log("Conexão com o banco realizada com sucesso");
+});
