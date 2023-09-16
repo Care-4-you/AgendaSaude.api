3b59d4067cdc92c57b7f7ccf65e86b8069786538
UR4N0-235 <matheus.fernandes92.235@protonmail.com>
2023-09-16 18:38:24 -0300

    feat (clinicas): added clinica model and schema

---
+++ src/models/clinica.model.ts
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+
+const clinicasSchema = new mongoose.Schema({
+  id: mongoose.Schema.Types.ObjectId,
+  nome: {
+    type: String,
+    required: true,
+  },
+  titulo: {
+    type: String,
+    required: true,
+  },
+  endereco: {
+    numero: {
+      type: String,
+      required: true,
+    },
+    rua: {
+      type: String,
+      required: true,
+    },
+    bairro: {
+      type: String,
+      required: true,
+    },
+    cidade: {
+      type: String,
+      required: true,
+    },
+    uf: {
+      type: String,
+      required: true,
+    },
+    cep: {
+      type: String,
+      required: true,
+    },
+    geo: {
+      lat: {
+        type: Number,
+        required: true,
+      },
+      lng: {
+        type: Number,
+        required: true,
+      },
+    },
+  },
+  imagem_url: [String],
+  contato: {
+    telefone: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+// Usando ES6 para o lint não brigar
+export const Clinicas = mongoose.model("clinicas", clinicasSchema);
